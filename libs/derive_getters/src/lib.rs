3f5a16379aa1f0b8146a3ec098ac1228104318a6
Lu Xugang <luxugang@apache.org>
2025-01-03 16:55:20 +0800

    Add Getter Marco

---
+++ libs/derive_getters/src/lib.rs
@@ -0,0 +1,65 @@
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput, Type};
+
+#[proc_macro_derive(Getters)]
+pub fn derive_getters(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let struct_name = &input.ident;
+    let fields = match &input.data {
+        syn::Data::Struct(data) => match &data.fields {
+            syn::Fields::Named(fields_named) => &fields_named.named,
+            _ => {
+                unimplemented!("Only named fields are supported by #[derive(Getters)]")
+            }
+        },
+        _ => {
+            unimplemented!("Only structs are supported by #[derive(Getters)]")
+        }
+    };
+
+    let getters = fields.iter().map(|field| {
+        let field_name = field.ident.as_ref().unwrap();
+        let field_type = &field.ty;
+        let getter_name = format_ident!("get_{}", field_name);
+
+        if is_copy_type(field_type) {
+            quote! {
+                pub fn #getter_name(&self) -> #field_type {
+                    self.#field_name
+                }
+            }
+        } else {
+            quote! {
+                pub fn #getter_name(&self) -> &#field_type {
+                    &self.#field_name
+                }
+            }
+        }
+    });
+
+    let expanded = quote! {
+        impl #struct_name {
+            #(#getters)*
+        }
+    };
+
+    TokenStream::from(expanded)
+}
+fn is_copy_type(ty: &Type) -> bool {
+    match ty {
+        Type::Path(type_path) => {
+            if let Some(segment) = type_path.path.segments.last() {
+                match segment.ident.to_string().as_str() {
+                    "bool" | "u8" | "u16" | "u32" | "u64" | "usize" | "i8" | "i16" | "i32"
+                    | "i64" | "isize" | "f32" | "f64" | "char" => true,
+                    _ => false,
+                }
+            } else {
+                false
+            }
+        }
+        _ => false,
+    }
+}
