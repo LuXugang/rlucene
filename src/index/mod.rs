a1d7770704103b3d80f71e70b13d7a6af68ff8c1
Lu Xugang <luxugang@apache.org>
2025-01-07 14:54:49 +0800

    Fix complie issue

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -37,7 +37,6 @@ pub mod numeric_doc_values;
 pub mod numeric_doc_values_field_updates;
 pub mod segment_commit_info;
 pub mod segment_info;
-pub mod segment_infos;
 pub mod sort;
 
 pub use index_file_names::*;
