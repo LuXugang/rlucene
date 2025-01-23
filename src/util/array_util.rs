e8f3a8d1db23d220ba2d3094b2528d6d9897e434
Lu Xugang <luxugang@apache.org>
2025-01-24 03:59:27 +0800

    Implemented the ArrayUtil

--- src/util/array_util.rs
+++ src/util/array_util.rs
@@ -14,7 +14,155 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use crate::util::error::lucene_error::LuceneError;
+use std::fmt::format;
+use std::mem;
+
 pub struct ArrayUtil;
 impl ArrayUtil {
     pub const MAX_ARRAY_LENGTH: i32 = 0;
+    const MIN_RADIX: i32 = 2;
+    const MAX_RADIX: i32 = 36;
+    /// Parses a char array into an i32 with the default radix of 10.
+    ///
+    /// # Arguments
+    ///
+    /// * `chars` - The character array to parse.
+    /// * `offset` - The starting offset in the array.
+    /// * `len` - The length of the array to parse.
+    ///
+    /// # Returns
+    ///
+    /// * `i32` - The parsed integer.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `LuceneError::NumberFormat` if it can't parse the chars into an integer.
+    fn parse_int_default(chars: &[char], offset: i32, len: i32) -> Result<i32, LuceneError> {
+        Self::parse_int(chars, offset, len, 10)
+    }
+
+    /// Parses the string argument as if it were an `i32` value and returns the result.
+    /// Throws a `LuceneError::NumberFormat` if the string does not represent an `i32` quantity.
+    /// The second argument specifies the radix to use when parsing the value.
+    fn parse_int(
+        chars: &[char],
+        mut offset: i32,
+        mut len: i32,
+        radix: i32,
+    ) -> Result<i32, LuceneError> {
+        if !(ArrayUtil::MIN_RADIX..=ArrayUtil::MAX_RADIX).contains(&radix) {
+            return Err(LuceneError::number_format("Invalid radix"));
+        }
+        if len == 0 {
+            return Err(LuceneError::number_format("chars length is 0"));
+        }
+        let mut i = 0;
+        let negative = chars[(offset + i) as usize] == '-';
+        if negative {
+            i += 1;
+            if i == len {
+                return Err(LuceneError::number_format("chars length is 0"));
+            }
+        }
+        if negative {
+            offset += 1;
+            len -= 1;
+        }
+        Self::parse(chars, offset, len, radix, negative)
+    }
+
+    fn parse(
+        chars: &[char],
+        offset: i32,
+        len: i32,
+        radix: i32,
+        negative: bool,
+    ) -> Result<i32, LuceneError> {
+        let max = i32::MIN / radix;
+        let mut result = 0;
+        for i in 0..len {
+            let digit = chars[(offset + i) as usize]
+                .to_digit(radix as u32)
+                .ok_or_else(|| LuceneError::number_format("Unable to parse"))?;
+            if max > result {
+                return Err(LuceneError::number_format("Unable to parse"));
+            }
+
+            let next = result
+                .checked_mul(radix as i32)
+                .and_then(|x| x.checked_sub(digit as i32));
+
+            match next {
+                Some(next) => {
+                    if next > result {
+                        return Err(LuceneError::number_format("Unable to parse"));
+                    }
+                    result = next;
+                }
+                None => return Err(LuceneError::number_format("Unable to parse")),
+            }
+        }
+        if !negative {
+            result = -result;
+            if result < 0 {
+                return Err(LuceneError::number_format("Unable to parse"));
+            }
+        }
+        Ok(result)
+    }
+    /// Calculates the new capacity after resizing.
+    ///
+    /// This method simply doubles the `min_target_size` to achieve the new capacity.
+    /// However, this is a basic resizing strategy that may not be suitable for all scenarios.
+    ///
+    /// Currently, `saturating_mul(2)` is used to avoid overflow, but if the new capacity exceeds `i32::MAX`,
+    /// it will return `i32::MAX`.
+    ///
+    /// In the future, this resizing strategy can be improved to adapt to different element sizes
+    /// or use a more intelligent growth factor.
+    ///
+    /// # Parameters
+    /// - `min_target_size`: The minimum desired target capacity.
+    /// - `_bytes_per_element`: The number of bytes per element (currently unused, reserved for future improvements).
+    ///
+    /// # Returns
+    /// The new capacity after resizing. If the result exceeds `i32::MAX`, `i32::MAX` is returned.
+    pub fn oversize(min_target_size: i32, _bytes_per_element: i32) -> i32 {
+        min_target_size.saturating_mul(2)
+    }
+    pub fn grow_exact<T>(vec: &mut Vec<T>, new_length: i32) -> Result<(), LuceneError> {
+        debug_assert!(
+            new_length >= 0,
+            "size must be positive (got {}): likely integer overflow?",
+            new_length
+        );
+        let current_len = vec.len();
+        if new_length as usize > current_len {
+            let additional = new_length as usize - current_len;
+            vec.reserve_exact(additional);
+        }
+        Ok(())
+    }
+    pub fn grow_with_len<T>(vec: &mut Vec<T>, min_size: i32) -> Result<(), LuceneError> {
+        debug_assert!(
+            min_size >= 0,
+            "size must be positive (got {}): likely integer overflow?",
+            min_size
+        );
+        let current_len = vec.len();
+        if min_size as usize > current_len {
+            let additional = min_size as usize - current_len;
+            vec.reserve(additional);
+        }
+        Ok(())
+    }
+    pub fn grow<T>(vec: &mut Vec<T>) -> Result<(), LuceneError> {
+        let bytes_per_element = mem::size_of::<T>();
+        debug_assert!(bytes_per_element <= i32::MAX as usize);
+        Self::grow_exact(
+            vec,
+            Self::oversize(vec.len() as i32, bytes_per_element as i32),
+        )
+    }
 }
