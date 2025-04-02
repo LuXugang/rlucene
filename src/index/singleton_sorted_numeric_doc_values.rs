7d3b96a71ea3346dfb77ae1d6d2a5d620737290e
Lu Xugang <luxugang@apache.org>
2025-04-02 17:22:57 +0800

    Implement SingletonSortedNumericDocValues

---
+++ src/index/singleton_sorted_numeric_doc_values.rs
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use crate::codecs::doc_values_enum::doc_values::NumericDocValuesEnum;
+use crate::index::doc_values_iterator::DocValuesIterator;
+use crate::index::numeric_doc_values::NumericDocValues;
+use crate::index::sorted_numeric_doc_values::SortedNumericDocValues;
+use crate::search::doc_id_set_iterator::DocIdSetIterator;
+use crate::store::IndexInput;
+use crate::util::error::lucene_error::LuceneError;
+use crate::util::error::lucene_error::Result;
+/// Exposes a multi-valued view over a single-valued instance.
+///
+/// This can be used if you want to have one multi-valued implementation that works for both
+/// single-valued and multi-valued types.
+pub struct SingletonSortedNumericDocValues<I>
+where
+    I: IndexInput,
+{
+    inner: NumericDocValuesEnum<I>,
+}
+
+impl<I> SingletonSortedNumericDocValues<I>
+where
+    I: IndexInput,
+{
+    pub fn new(inner: NumericDocValuesEnum<I>) -> Result<Self> {
+        if inner.doc_id() != -1 {
+            return Err(LuceneError::illegal_state(format!(
+                "iterator has already been used: docID={}",
+                inner.doc_id()
+            )));
+        }
+        Ok(Self { inner })
+    }
+
+    pub fn get_numeric_doc_values(&self) -> Result<&NumericDocValuesEnum<I>> {
+        if self.inner.doc_id() != -1 {
+            return Err(LuceneError::illegal_state(format!(
+                "iterator has already been used: docID={}",
+                self.inner.doc_id()
+            )));
+        }
+        Ok(&self.inner)
+    }
+}
+
+impl<I> DocIdSetIterator for SingletonSortedNumericDocValues<I>
+where
+    I: IndexInput,
+{
+    fn doc_id(&self) -> i32 {
+        self.inner.doc_id()
+    }
+
+    fn next_doc(&mut self) -> Result<i32> {
+        self.inner.next_doc()
+    }
+
+    fn advance(&mut self, target: i32) -> Result<i32> {
+        self.inner.advance(target)
+    }
+
+    fn cost(&self) -> Result<i64> {
+        self.inner.cost()
+    }
+}
+
+impl<I> DocValuesIterator for SingletonSortedNumericDocValues<I>
+where
+    I: IndexInput,
+{
+    fn advance_exact(&mut self, target: i32) -> Result<bool> {
+        self.inner.advance_exact(target)
+    }
+}
+
+impl<I> SortedNumericDocValues for SingletonSortedNumericDocValues<I>
+where
+    I: IndexInput,
+{
+    fn next_value(&mut self) -> Result<i64> {
+        self.inner.long_value()
+    }
+
+    fn doc_value_count(&mut self) -> Result<i32> {
+        Ok(1)
+    }
+}
