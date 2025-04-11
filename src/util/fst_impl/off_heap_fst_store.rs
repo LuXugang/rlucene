eb06dea81df93542793c2217f6ff1bc84dc36c55
Lu Xugang <luxugang@apache.org>
2025-04-12 03:38:11 +0800

    Implement OffHeapFSTStore

--- src/util/fst_impl/off_heap_fst_store.rs
+++ src/util/fst_impl/off_heap_fst_store.rs
@@ -14,27 +14,63 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use crate::store::IndexInput;
-use std::sync::{Arc, Mutex};
-#[allow(unused)]
+use crate::store::{DataOutput, IndexInput};
+use crate::util::accountable::Accountable;
+use crate::util::error::lucene_error::{LuceneError, Result};
+use crate::util::fst_impl::fst_reader::FstReader;
+use crate::util::fst_impl::reverse_random_access_reader::ReverseRandomAccessReader;
+use std::cell::RefCell;
+use std::rc::Rc;
+/// Provides off heap storage of finite state machine (FST), using underlying index input instead of
+///  byte store on heap
 pub struct OffHeapFSTStore<I>
 where
     I: IndexInput,
 {
-    input: Arc<Mutex<I>>,
+    input: Rc<RefCell<I>>,
     offset: i64,
     num_bytes: i64,
 }
-#[allow(unused)]
 impl<I> OffHeapFSTStore<I>
 where
     I: IndexInput,
 {
-    pub fn new(input: Arc<Mutex<I>>, offset: i64, num_bytes: i64) -> Self {
+    pub fn new(input: Rc<RefCell<I>>, offset: i64, num_bytes: i64) -> Self {
         Self {
             input,
             offset,
             num_bytes,
         }
+    }
+    pub fn size(&self) -> i64 {
+        self.num_bytes
+    }
+}
+
+impl<I> Accountable for OffHeapFSTStore<I>
+where
+    I: IndexInput,
+{
+    fn ram_bytes_used(&self) -> Result<i64> {
+        todo!()
+    }
+}
+
+impl<I> FstReader for OffHeapFSTStore<I>
+where
+    I: IndexInput,
+{
+    type FstBytesReader = ReverseRandomAccessReader<I::RandomAccessSlice>;
+
+    fn get_reverse_bytes_reader(&self) -> Result<Self::FstBytesReader> {
+        let input = self.input.borrow_mut();
+        let slice = input.random_access_slice(self.offset, self.num_bytes)?;
+        Ok(ReverseRandomAccessReader::new(slice))
+    }
+
+    fn write_to(&self, _out: &mut impl DataOutput) -> Result<()> {
+        Err(LuceneError::unsupported_operation(
+            "write_to is not supported for OffHeapFSTStore",
+        ))
     }
 }
