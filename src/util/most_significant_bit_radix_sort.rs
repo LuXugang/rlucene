228ad4e95d2f28038d751abf9780378f618d4973
Lu Xugang <luxugang@apache.org>
2025-05-12 23:23:17 +0800

    Remove dead code

--- src/util/most_significant_bit_radix_sort.rs
+++ src/util/most_significant_bit_radix_sort.rs
@@ -252,8 +252,6 @@ where
         self.reorder(from, to, l, k)?;
 
         // Update end offsets
-        // TODO: could we avoid copy here?
-        self.histograms[l as usize] = self.end_offsets.clone();
 
         // Recursively sort buckets if more levels are allowed
         if k + 1 < self.max_length {
