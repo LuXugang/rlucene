843ab412b15354a50959c1ae66262059fd9cf13f
Lu Xugang <luxugang@apache.org>
2025-05-28 21:04:27 +0800

    Implement ScoreDoc

--- src/search/score_doc.rs
+++ src/search/score_doc.rs
@@ -14,7 +14,46 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#[allow(unused)]
+use std::fmt;
+
+/// Holds one hit in [`TopDocs`](crate::search::top_docs::TopDocs).
+#[derive(Debug, Clone, PartialEq)]
 pub struct ScoreDoc {
+    /// The score of this document for the query.
+    pub score: f32,
+
+    /// A hit document's number.
+    ///
+    /// See [`StoredFields::document`](crate::index::stored_fields::StoredFields::document).
     pub doc: i32,
+
+    /// Only set by `TopDocs::merge`.
+    pub shard_index: i32,
+}
+
+impl ScoreDoc {
+    /// Constructs a `ScoreDoc`.
+    pub fn new(doc: i32, score: f32) -> Self {
+        Self::with_shard_index(doc, score, -1)
+    }
+
+    /// Constructs a `ScoreDoc` with a given `shard_index`.
+    pub fn with_shard_index(doc: i32, score: f32, shard_index: i32) -> Self {
+        Self {
+            doc,
+            score,
+            shard_index,
+        }
+    }
+}
+
+impl fmt::Display for ScoreDoc {
+    /// A convenience method for debugging.
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "doc={} score={} shardIndex={}",
+            self.doc, self.score, self.shard_index
+        )
+    }
 }
