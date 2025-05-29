91f08a42b7e4d82cfa9920feaa161b46cf4c1748
Lu Xugang <luxugang@apache.org>
2025-05-29 16:52:40 +0800

    Implement KnnVectorValues

--- src/index/knn_vector_values.rs
+++ src/index/knn_vector_values.rs
@@ -14,10 +14,90 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+use crate::index::vector_encoding::VectorEncoding;
 use crate::search::doc_id_set_iterator::disi_const::NO_MORE_DOCS;
 use crate::search::doc_id_set_iterator::DocIdSetIterator;
-use crate::util::error::lucene_error::Result;
+use crate::util::bits::Bits;
+use crate::util::error::lucene_error::{LuceneError, Result};
+/// This struct abstracts addressing of document vector values indexed as
+/// [`KnnFloatVectorField`](crate::document::knn_float_vector_field::KnnFloatVectorField) or [`KnnByteVectorField`](crate::document::knn_byte_vector_field::KnnByteVectorField).
+pub trait KnnVectorValues {
+    /// Return the dimension of the vectors
+    fn dimension(&self) -> i32;
+    /// Return the number of vectors for this field.
+    ///
+    /// # Returns
+    /// The number of vectors returned by this iterator.
+    fn size(&self) -> i32;
+    /// Return the docid of the document indexed with the given vector ordinal.
+    /// This default implementation returns the argument and is appropriate for
+    /// dense values implementations where every doc has a single value.
+    fn ord_to_doc(&self, ord: i32) -> i32 {
+        ord
+    }
+    /// Creates a new copy of this [`KnnVectorValues`]. This is helpful when you
+    /// need to access different values at once, to avoid overwriting the
+    /// underlying vector returned.
+    fn copy(&self) -> Result<Self>
+    where
+        Self: Sized;
+    /// Returns the vector byte length, defaults to dimension multiplied by
+    /// float byte size
+    fn get_vector_byte_length(&self) -> usize {
+        (self.dimension() * self.get_encoding().byte_size()) as usize
+    }
+    /// The vector encoding of these values.
+    fn get_encoding(&self) -> VectorEncoding;
+
+    type Bits: Bits;
+    /// Returns a Bits accepting docs accepted by the argument and having a
+    /// vector value
+    fn get_accept_ords<B>(&self, accept_docs: B) -> Self::Bits
+    where
+        B: Bits;
+
+    type DocIndexIterator: DocIndexIterator;
+    ///  Create an iterator for this instance.
+    fn iterator(&self) -> Result<Self::DocIndexIterator> {
+        Err(LuceneError::unsupported_operation(""))
+    }
+}
+
+pub(crate) struct BitsImpl<B, T>
+where
+    B: Bits,
+    T: OrdToDoc,
+{
+    accept_docs: B,
+    size: i32,
+    map: T,
+}
+impl<B, T> BitsImpl<B, T>
+where
+    B: Bits,
+    T: OrdToDoc,
+{
+    pub(crate) fn new(accept_docs: B, size: i32, map: T) -> Self {
+        Self {
+            accept_docs,
+            size,
+            map,
+        }
+    }
+}
+impl<B, T> Bits for BitsImpl<B, T>
+where
+    B: Bits,
+    T: OrdToDoc,
+{
+    fn get(&self, index: i32) -> bool {
+        self.accept_docs.get(self.map.ord_to_doc(index))
+    }
+
+    fn length(&self) -> i32 {
+        self.size
+    }
+}
 
 /// A DocIdSetIterator that also provides an index() method tracking a distinct
 /// ordinal for a vector associated with each doc.
