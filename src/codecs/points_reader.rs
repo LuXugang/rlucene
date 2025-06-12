e636e082fad92dbb0fb2f5c76bbeb88dce69b230
Lu Xugang <luxugang@apache.org>
2025-06-13 00:12:29 +0800

    Add PointsReader

---
+++ src/codecs/points_reader.rs
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use crate::index::point_values::{PointValues, PointValuesBase};
+use crate::util::error::lucene_error::Result;
+/// Abstract API to visit point values.
+pub trait PointsReader {
+    /// Checks consistency of this reader.
+    ///
+    /// Note that this may be costly in terms of I/O, e.g. may involve computing
+    /// a checksum value against large data files.
+    fn check_integrity(&mut self) -> Result<()>;
+
+    type PointValuesBase: PointValuesBase;
+    fn get_values(&mut self, field: &str) -> PointValues<Self::PointValuesBase>;
+
+    /// Returns an instance optimized for merging. This instance may only be
+    /// cloned
+    /// # Note
+    /// Returning None means returning itself.
+    fn get_merge_instance(&self) -> Result<Option<()>> {
+        // TODO: 返回值不对
+        Ok(None)
+    }
+}
