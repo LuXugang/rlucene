db426018a59001c5679bd7f289748f54ac77928b
Lu Xugang <luxugang@apache.org>
2025-06-18 08:45:07 +0800

    Implement BufferedNorms

--- src/index/norm_values_writer.rs
+++ src/index/norm_values_writer.rs
@@ -14,14 +14,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use crate::index::doc_values_iterator::DocValuesIterator;
 use crate::index::docs_with_field_set::DocsWithFieldSetEnum;
-use crate::util::error::lucene_error::Result;
+use crate::index::numeric_doc_values::NumericDocValues;
+use crate::search::doc_id_set_iterator::disi_const::NO_MORE_DOCS;
+use crate::search::doc_id_set_iterator::DocIdSetIterator;
+use crate::util::error::lucene_error::{LuceneError, Result};
 use crate::util::packed::packed_long_values::{PackedLongValues, PackedLongValuesIterator};
 pub struct NormValuesWriter;
 
 struct BufferedNorms<'a> {
     iter: PackedLongValuesIterator<'a>,
     doc_with_field: DocsWithFieldSetEnum<'a>,
+    value: i64,
 }
 impl<'a> BufferedNorms<'a> {
     pub(crate) fn new(
@@ -31,6 +36,41 @@ impl<'a> BufferedNorms<'a> {
         Ok(Self {
             iter: values.iterator()?,
             doc_with_field,
+            value: 0,
         })
+    }
+}
+
+impl DocValuesIterator for BufferedNorms<'_> {
+    fn advance_exact(&mut self, _target: i32) -> Result<bool> {
+        Err(LuceneError::unsupported_operation(""))
+    }
+}
+
+impl DocIdSetIterator for BufferedNorms<'_> {
+    fn doc_id(&self) -> i32 {
+        self.doc_with_field.doc_id()
+    }
+
+    fn next_doc(&mut self) -> Result<i32> {
+        let doc_id = self.doc_with_field.next_doc()?;
+        if doc_id != NO_MORE_DOCS {
+            self.value = self.iter.next_value()?;
+        }
+        Ok(doc_id)
+    }
+
+    fn advance(&mut self, _target: i32) -> Result<i32> {
+        Err(LuceneError::unsupported_operation(""))
+    }
+
+    fn cost(&self) -> Result<i64> {
+        self.doc_with_field.cost()
+    }
+}
+
+impl<'a> NumericDocValues for BufferedNorms<'a> {
+    fn long_value(&mut self) -> Result<i64> {
+        Ok(self.value)
     }
 }
