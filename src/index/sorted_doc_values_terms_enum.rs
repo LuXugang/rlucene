7b921f80c6d160e60bcd23c5604551111209d091
Lu Xugang <luxugang@apache.org>
2025-06-19 10:32:42 +0800

    Implement native SortedDocValuesTermsEnum

--- src/index/sorted_doc_values_terms_enum.rs
+++ src/index/sorted_doc_values_terms_enum.rs
@@ -14,162 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use crate::index::dummy::dummy_impacts_enum::DummyImpactsEnum;
+use crate::index::dummy::dummy_postings_enum::DummyPostingsEnum;
+use crate::index::dummy::dummy_term_state_type::DummyTermState;
+use crate::index::terms_enum::TermsEnum;
+use crate::util::bytes_ref_iterator::BytesRefIterator;
 
 /// Implements a [`TermsEnum`](TermsEnum) wrapping a provided
 /// [`SortedDocValues`](SortedDocValues).
 pub struct SortedDocValuesTermsEnum;
-// where
-//     I: IndexInput,
-//     AV: AccessVec<u8>,
-// {
-//     values: SortedDocValues<I, AV>,
-//     current_ord: i32,
-//     bytes: BytesRef<AV>,
-// }
-//
-// impl<I, AV> SortedDocValuesTermsEnum<I, AV>
-// where
-//     I: IndexInput,
-//     AV: AccessVec<u8>,
-// {
-//     /// Creates a new TermsEnum over the provided values.
-//     pub fn new(values: SortedDocValuesEnum<I, AV>) -> Self {
-//         Self {
-//             values,
-//             current_ord: -1,
-//             bytes: BytesRef::new(),
-//         }
-//     }
-// }
-//
-// impl<I, AV> BytesRefIterator for SortedDocValuesTermsEnum<I, AV>
-// where
-//     I: IndexInput,
-//     AV: AccessVec<u8>,
-// {
-//     fn next(&mut self) -> Result<Option<Cow<BytesRef<Vec<u8>>>>> {
-//         self.current_ord += 1;
-//         if self.current_ord >= self.values.get_value_count()? {
-//             Ok(None)
-//         } else {
-//             match self.values.lookup_ord(self.current_ord)? {
-//                 Cow::Owned(bytes) => {
-//                     self.bytes = bytes;
-//                 }
-//                 Cow::Borrowed(bytes) => {
-//                     self.bytes = bytes.clone();
-//                 }
-//             }
-//             Ok(Some(Cow::Borrowed(&self.bytes)))
-//         }
-//     }
-// }
-//
-// impl<I, AV> TermsEnum<AV> for SortedDocValuesTermsEnum<I, AV>
-// where
-//     I: IndexInput,
-//     AV: AccessVec<u8>,
-// {
-//     fn attributes(&self) -> Result<&AttributeSource> {
-//         Err(LuceneError::not_implemented(""))
-//     }
-//
-//     fn seek_exact(&mut self, text: &BytesRef<AV>) -> Result<bool> {
-//         let ord = self.values.lookup_term(text)?;
-//         if ord >= 0 {
-//             self.current_ord = ord;
-//             self.bytes = text.clone();
-//             Ok(true)
-//         } else {
-//             Ok(false)
-//         }
-//     }
-//
-//     fn seek_ceil(&mut self, text: &BytesRef<AV>) -> Result<SeekStatus> {
-//         let ord = self.values.lookup_term(text)?;
-//         if ord >= 0 {
-//             self.current_ord = ord;
-//             self.bytes = text.clone();
-//             Ok(SeekStatus::Found)
-//         } else {
-//             self.current_ord = -ord - 1;
-//             if self.current_ord == self.values.get_value_count()? {
-//                 Ok(SeekStatus::End)
-//             } else {
-//                 // TODO: hmm, can we avoid this extra lookup?
-//                 match self.values.lookup_ord(self.current_ord)? {
-//                     Cow::Owned(bytes) => {
-//                         self.bytes = bytes;
-//                     }
-//                     Cow::Borrowed(bytes) => {
-//                         self.bytes = bytes.clone();
-//                     }
-//                 }
-//                 Ok(SeekStatus::NotFound)
-//             }
-//         }
-//     }
-//
-//     fn seek_exact_with_ord(&mut self, ord: i64) -> Result<()> {
-//         debug_assert!(ord >= 0 && ord < self.values.get_value_count()? as
-// i64);         self.current_ord = ord as i32;
-//         match self.values.lookup_ord(self.current_ord)? {
-//             Cow::Owned(bytes) => {
-//                 self.bytes = bytes;
-//             }
-//             Cow::Borrowed(bytes) => {
-//                 self.bytes = bytes.clone();
-//             }
-//         }
-//         Ok(())
-//     }
-//
-//     fn seek_exact_with_state(&mut self, _term: &BytesRef<AV>, state:
-// &TermStateEnum) -> Result<()> {         debug_assert!({ matches!(state,
-// TermStateEnum::Ord(_)) });         match state {
-//             TermStateEnum::Ord(ord_term_state) =>
-// self.seek_exact_with_ord(ord_term_state.ord)?,             _ => return
-// Err(LuceneError::illegal_state("state should be OrdTermState")),         }
-//         Ok(())
-//     }
-//
-//     fn term(&self) -> Result<Cow<BytesRef<AV>>> {
-//         Ok(Cow::Borrowed(&self.bytes))
-//     }
-//
-//     fn ord(&self) -> Result<i64> {
-//         Ok(self.current_ord as i64)
-//     }
-//
-//     fn doc_freq(&mut self) -> Result<i32> {
-//         Err(LuceneError::unsupported_operation(""))
-//     }
-//
-//     fn total_term_freq(&mut self) -> Result<i64> {
-//         Err(LuceneError::unsupported_operation(""))
-//     }
-//
-//     type PostingsEnum = DummyPostingsEnum;
-//
-//     fn postings_with_flags(
-//         &mut self,
-//         _reuse: Option<Self::PostingsEnum>,
-//         _flags: i32,
-//     ) -> Result<Self::PostingsEnum> {
-//         Err(LuceneError::unsupported_operation(""))
-//     }
-//
-//     type ImpactsEnum = DummyImpactsEnum;
-//
-//     fn impacts(&mut self, _flags: i32) -> Result<Self::ImpactsEnum> {
-//         Err(LuceneError::unsupported_operation(""))
-//     }
-//
-//     type TermState = TermStateEnum;
-//
-//     fn term_state(&mut self) -> Result<Self::TermState> {
-//         let mut state = OrdTermState::default();
-//         state.ord = self.current_ord as i64;
-//         Ok(TermStateEnum::Ord(state))
-//     }
-// }
+
+impl BytesRefIterator for SortedDocValuesTermsEnum {}
+
+impl TermsEnum for SortedDocValuesTermsEnum {
+    type PostingsEnum = DummyPostingsEnum;
+    type ImpactsEnum = DummyImpactsEnum;
+    type TermState = DummyTermState;
+}
