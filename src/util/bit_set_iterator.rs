023619e1e423b315f862101386ff0f820f30fa79
Lu Xugang <luxugang@apache.org>
2025-06-25 23:38:27 +0800

    Add bsi_util mod

--- src/util/bit_set_iterator.rs
+++ src/util/bit_set_iterator.rs
@@ -18,9 +18,6 @@ use crate::search::doc_id_set_iterator::disi_const::NO_MORE_DOCS;
 use crate::search::doc_id_set_iterator::DocIdSetIterator;
 use crate::util::bit_set::BitSet;
 use crate::util::error::lucene_error::{LuceneError, Result};
-use crate::util::fixed_bit_set::FixedBitSet;
-use crate::util::sparse_fixed_bit_set::SparseFixedBitSet;
-use std::any::TypeId;
 use std::rc::Rc;
 
 /// A [`DocIdSetIterator`] which iterates over set bits in a bit set.
@@ -53,16 +50,46 @@ impl<T: BitSet> BitSetIterator<T> {
             doc: -1,
         })
     }
-
-    pub fn get_bit_set(&self) -> Rc<T> {
-        self.bits.clone()
-    }
-
     // Set the current doc id that this iterator is on.
     #[allow(unused)]
     fn set_doc_id(&mut self, doc_id: i32) {
         self.doc = doc_id;
     }
+    pub fn get_bit_set(&self) -> Rc<T> {
+        self.bits.clone()
+    }
+}
+
+impl<T: BitSet> DocIdSetIterator for BitSetIterator<T> {
+    fn doc_id(&self) -> i32 {
+        self.doc
+    }
+
+    fn next_doc(&mut self) -> Result<i32> {
+        self.advance(self.doc + 1)
+    }
+
+    fn advance(&mut self, _target: i32) -> Result<i32> {
+        if _target >= self.length {
+            self.doc = NO_MORE_DOCS;
+            return Ok(self.doc);
+        }
+        self.doc = self.bits.next_set_bit(_target);
+        Ok(self.doc)
+    }
+
+    fn cost(&self) -> Result<i64> {
+        Ok(self.cost)
+    }
+}
+
+pub mod bsi_util {
+    use crate::search::doc_id_set_iterator::DocIdSetIterator;
+    use crate::util::bit_set::BitSet;
+    use crate::util::fixed_bit_set::FixedBitSet;
+    use crate::util::sparse_fixed_bit_set::SparseFixedBitSet;
+    use std::any::TypeId;
+
     #[allow(unused)]
     fn equal_disi_type<T1: DocIdSetIterator + 'static, T2: DocIdSetIterator + 'static>(
         _it1: &T1,
@@ -91,7 +118,7 @@ impl<T: BitSet> BitSetIterator<T> {
     pub fn get_fixed_bit_set_or_null<B: BitSet>(
         iterator: impl DocIdSetIterator + 'static,
     ) -> Option<FixedBitSet> {
-        Self::try_get_bit_set(iterator, FixedBitSet::default())
+        todo!()
     }
 
     // todo
@@ -100,29 +127,6 @@ impl<T: BitSet> BitSetIterator<T> {
     pub fn get_sparse_fixed_bit_set_or_null<B: BitSet>(
         iterator: impl DocIdSetIterator + 'static,
     ) -> Option<SparseFixedBitSet> {
-        Self::try_get_bit_set(iterator, SparseFixedBitSet::default())
-    }
-}
-
-impl<T: BitSet> DocIdSetIterator for BitSetIterator<T> {
-    fn doc_id(&self) -> i32 {
-        self.doc
-    }
-
-    fn next_doc(&mut self) -> Result<i32> {
-        self.advance(self.doc + 1)
-    }
-
-    fn advance(&mut self, _target: i32) -> Result<i32> {
-        if _target >= self.length {
-            self.doc = NO_MORE_DOCS;
-            return Ok(self.doc);
-        }
-        self.doc = self.bits.next_set_bit(_target);
-        Ok(self.doc)
-    }
-
-    fn cost(&self) -> Result<i64> {
-        Ok(self.cost)
+        todo!()
     }
 }
