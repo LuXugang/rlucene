ad1711b39ba7a07a15abb0bf173f9564bdd4e93f
Lu Xugang <luxugang@apache.org>
2025-08-25 06:07:33 +0800

    Make world quite under debug coding

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#![cfg_attr(debug_assertions, allow(dead_code, unused_variables))]
 #![allow(deprecated)]
 pub mod analysis;
 pub mod codecs;
