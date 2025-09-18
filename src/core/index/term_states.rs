9cf497220af167ba8207e2e104e80baa0e3e639a
Lu Xugang <luxugang@apache.org>
2025-09-19 06:13:13 +0800

    Add more method in TermStates

--- src/core/index/term_states.rs
+++ src/core/index/term_states.rs
@@ -17,9 +17,13 @@
 use crate::core::index::index_reader_context::IndexReaderContext;
 use crate::core::index::term::Term;
 use crate::core::index::term_state::TermState;
-use crate::core::util::error::lucene_error::Result;
+use crate::core::util::error::lucene_error::{LuceneError, Result};
+use std::fmt::{Display, Formatter};
 use std::rc::Rc;
-
+/// Maintains an [`IndexReader`](crate::core::index::index_reader::IndexReader) [`TermState`] view over [`IndexReader`](crate::core::index::index_reader::IndexReader) instances
+/// containing a single term. The [`TermStates`] doesn't track if the given [`TermState`]
+/// objects are valid, neither if the [`TermState`] instances refer to the same terms in the
+/// associated readers.
 pub struct TermStates<TS>
 where
     TS: TermState + Default,
@@ -54,11 +58,47 @@ where
     pub fn was_built_for(&self, context: &impl IndexReaderContext) -> bool {
         Rc::ptr_eq(&self.top_reader_context_identity, &context.base().identity)
     }
+    pub fn new_with_state_and_stats(
+        context: &impl IndexReaderContext,
+        state: TS,
+        ord: usize,
+        doc_freq: i32,
+        total_term_freq: i64,
+    ) -> Result<Self> {
+        let mut ts = TermStates::new_empty(context)?;
+        ts.register_with_stats(state, ord, doc_freq, total_term_freq);
+        Ok(ts)
+    }
 
+    /// Clears the TermStates internal state and removes all registered TermStates
+    pub fn clear(&mut self) {
+        self.doc_freq = 0;
+        self.total_term_freq = 0;
+
+        for slot in &mut self.states {
+            *slot = TS::default();
+        }
+    }
+    /// Registers and associates a TermState with an leaf ordinal.
+    /// The leaf ordinal should be derived from a IndexReaderContext's leaf ord.
+    pub fn register_with_stats(
+        &mut self,
+        state: TS,
+        ord: usize,
+        doc_freq: i32,
+        total_term_freq: i64,
+    ) {
+        self.register(state, ord);
+        self.accumulate_statistics(doc_freq, total_term_freq);
+    }
+    /// Expert: Registers and associates a [`TermState`] with a leaf ordinal.
+    /// The leaf ordinal should be derived from an [`IndexReaderContext`]'s leaf ord.
+    /// Unlike [`register`](Self::register_with_stats), this method does **not** update term statistics.
     pub fn register(&mut self, state: TS, ord: usize) {
         debug_assert!(ord < self.states.len(), "ord {} out of bounds", ord);
         self.states[ord] = state;
     }
+    /// Expert: Accumulate term statistics.
     pub fn accumulate_statistics(&mut self, doc_freq: i32, total_term_freq: i64) {
         debug_assert!(doc_freq >= 0);
         debug_assert!(total_term_freq >= 0);
@@ -68,5 +108,45 @@ where
         );
         self.doc_freq += doc_freq;
         self.total_term_freq += total_term_freq;
+    }
+    /// Returns the accumulated document frequency of all [`TermState`] instances
+    /// passed to [`register(TermState, int, int, long)`].
+    ///
+    /// # Returns
+    /// The accumulated document frequency of all [`TermState`] instances passed
+    /// to [`register(TermState, int, int, long)`].
+    pub fn doc_freq(&self) -> Result<i32> {
+        if self.term.is_some() {
+            return Err(LuceneError::illegal_state(
+                "Cannot call doc_freq() when needsStats=false",
+            ));
+        }
+        Ok(self.doc_freq)
+    }
+    /// Returns the accumulated term frequency of all [`TermState`] instances
+    /// passed to [`register`](Self::register_with_stats).
+    ///
+    /// # Returns
+    /// The accumulated term frequency of all [`TermState`] instances passed
+    /// to [`register`](Self::register_with_stats).
+    pub fn total_term_freq(&self) -> Result<i64> {
+        if self.term.is_some() {
+            return Err(LuceneError::illegal_state(
+                "Cannot call total_term_freq() when needsStats=false",
+            ));
+        }
+        Ok(self.total_term_freq)
+    }
+}
+impl<TS> Display for TermStates<TS>
+where
+    TS: TermState + Default,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "TermStates")?;
+        for state in &self.states {
+            writeln!(f, "  state={}", state)?;
+        }
+        Ok(())
     }
 }
