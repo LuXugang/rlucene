1ca62801aeb191278b3812521174c551b696ac15
Lu Xugang <luxugang@apache.org>
2025-10-11 18:11:52 +0800

    comparators should be  FieldComparatorEnum

--- src/core/search/field_value_hit_queue.rs
+++ src/core/search/field_value_hit_queue.rs
@@ -14,18 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use crate::core::index::leaf_reader::LeafReader;
-use crate::core::search::leaf_field_comparator::LeafFieldComparatorEnum;
+use crate::core::search::field_comparator::FieldComparatorEnum;
 use crate::core::search::score_doc::{ScoreDoc, ScoreDocLike};
 use crate::core::search::sort_field::SortField;
 use std::fmt;
 
-pub struct FieldValueHitQueue<LR>
-where
-    LR: LeafReader,
-{
+pub struct FieldValueHitQueue {
     pub(crate) fields: Vec<SortField>,
-    comparators: Vec<LeafFieldComparatorEnum<LR>>,
+    comparators: Vec<FieldComparatorEnum>,
     reverse_mul: Vec<i32>,
 }
 
