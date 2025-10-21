dbb6e55040fd208a0504e1e70a4321137ea14e40
Lu Xugang <luxugang@apache.org>
2025-10-22 00:22:21 +0800

    Add IndexReaderEnum

--- src/core/index/index_reader.rs
+++ src/core/index/index_reader.rs
@@ -14,11 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use crate::core::index::stored_fields::StoredFields;
+use crate::core::index::codec_reader::CodecReader;
+use crate::core::index::composite_reader::CompositeReader;
+use crate::core::index::leaf_reader::LeafReader;
+use crate::core::index::stored_fields::{Either2StoredFields, StoredFields};
 use crate::core::index::term::Term;
-use crate::core::index::term_vectors::TermVectors;
+use crate::core::index::term_vectors::{Either2TermVectors, TermVectors};
 use crate::core::util::error::lucene_error::Result;
-use std::fmt::Display;
+use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
@@ -120,5 +123,146 @@ impl Eq for CacheKey {}
 impl Hash for CacheKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
         (Arc::as_ptr(&self.identity) as usize).hash(state);
+    }
+}
+
+pub type IRTermVectors<LR, CR> =
+    Either2TermVectors<<LR as IndexReader>::TermVectors, <CR as IndexReader>::TermVectors>;
+pub type IRStoredFields<LR, CR> =
+    Either2StoredFields<<LR as IndexReader>::StoredFields, <CR as IndexReader>::StoredFields>;
+pub enum IndexReaderEnum<LR, CR>
+where
+    LR: LeafReader,
+    CR: CompositeReader,
+{
+    Leaf(LR),
+    Composite(CR),
+}
+
+impl<LR, CR> Display for IndexReaderEnum<LR, CR>
+where
+    CR: CompositeReader,
+    LR: LeafReader,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => write!(f, "LeafReader: {}", leaf),
+            IndexReaderEnum::Composite(comp) => write!(f, "CompositeReader: {}", comp),
+        }
+    }
+}
+
+impl<LR, CR> IndexReader for IndexReaderEnum<LR, CR>
+where
+    LR: LeafReader,
+    CR: CompositeReader,
+{
+    type TermVectors = IRTermVectors<LR, CR>;
+
+    fn term_vectors(&self) -> Result<Self::TermVectors> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => Ok(Either2TermVectors::A(leaf.term_vectors()?)),
+            IndexReaderEnum::Composite(comp) => Ok(Either2TermVectors::B(comp.term_vectors()?)),
+        }
+    }
+
+    fn max_doc(&self) -> Result<i32> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.max_doc(),
+            IndexReaderEnum::Composite(comp) => comp.max_doc(),
+        }
+    }
+
+    fn num_docs(&self) -> Result<i32> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.num_docs(),
+            IndexReaderEnum::Composite(comp) => comp.num_docs(),
+        }
+    }
+
+    fn num_deleted_docs(&self) -> Result<i32> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.num_deleted_docs(),
+            IndexReaderEnum::Composite(comp) => comp.num_deleted_docs(),
+        }
+    }
+
+    fn inc_ref(&self) -> Result<()> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.inc_ref(),
+            IndexReaderEnum::Composite(comp) => comp.inc_ref(),
+        }
+    }
+
+    fn dec_ref(&self) -> Result<()> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.dec_ref(),
+            IndexReaderEnum::Composite(comp) => comp.dec_ref(),
+        }
+    }
+
+    fn ensure_open(&self) -> Result<()> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.ensure_open(),
+            IndexReaderEnum::Composite(comp) => comp.ensure_open(),
+        }
+    }
+
+    type StoredFields = IRStoredFields<LR, CR>;
+
+    fn stored_fields(&self) -> Result<Self::StoredFields> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => Ok(Either2StoredFields::A(leaf.stored_fields()?)),
+            IndexReaderEnum::Composite(comp) => Ok(Either2StoredFields::B(comp.stored_fields()?)),
+        }
+    }
+
+    fn has_deletions(&self) -> Result<bool> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.has_deletions(),
+            IndexReaderEnum::Composite(comp) => comp.has_deletions(),
+        }
+    }
+
+    fn do_close(&mut self) -> Result<()> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.do_close(),
+            IndexReaderEnum::Composite(comp) => comp.do_close(),
+        }
+    }
+
+    fn doc_freq(&self, term: &Term) -> Result<i32> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => <LR as IndexReader>::doc_freq(leaf, term),
+            IndexReaderEnum::Composite(comp) => comp.doc_freq(term),
+        }
+    }
+
+    fn total_term_freq(&self, term: &Term) -> Result<i64> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => <LR as IndexReader>::total_term_freq(leaf, term),
+            IndexReaderEnum::Composite(comp) => comp.total_term_freq(term),
+        }
+    }
+
+    fn get_sum_doc_freq(&self, field: &str) -> Result<i64> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.get_sum_doc_freq(field),
+            IndexReaderEnum::Composite(comp) => comp.get_sum_doc_freq(field),
+        }
+    }
+
+    fn get_doc_count(&self, field: &str) -> Result<i32> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.get_doc_count(field),
+            IndexReaderEnum::Composite(comp) => comp.get_doc_count(field),
+        }
+    }
+
+    fn get_sum_total_term_freq(&self, field: &str) -> Result<i64> {
+        match self {
+            IndexReaderEnum::Leaf(leaf) => leaf.get_sum_total_term_freq(field),
+            IndexReaderEnum::Composite(comp) => comp.get_sum_total_term_freq(field),
+        }
     }
 }
