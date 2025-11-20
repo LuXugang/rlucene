42a6ba38467a9af1da80e93d08b8113919656a5c
Lu Xugang <luxugang@apache.org>
2025-11-20 14:48:28 +0800

    Implement TestDocsAndPositions

--- src/test/index/test_docs_and_positions.rs
+++ src/test/index/test_docs_and_positions.rs
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 use crate::core::document::document::Document;
+use crate::core::document::field::Store::No;
 use crate::core::document::field_type::FieldType;
 use crate::core::document::text_field::text_field_type;
 use crate::core::index::BytesRef;
@@ -22,7 +23,8 @@ use crate::core::index::composite_reader::get_context;
 use crate::core::index::index_reader::IndexReader;
 use crate::core::index::index_reader_context::IndexReaderContext;
 use crate::core::index::leaf_reader::{LRPosting, LeafReader};
-use crate::core::index::postings_enum::{ALL, PostingsEnum};
+use crate::core::index::postings_enum::{ALL, Either2PostingsEnum, PostingsEnum};
+use crate::core::index::term::Term;
 use crate::core::index::terms::Terms;
 use crate::core::index::terms_enum::TermsEnum;
 use crate::core::search::doc_id_set_iterator::DocIdSetIterator;
@@ -30,7 +32,8 @@ use crate::core::search::doc_id_set_iterator::disi_const::NO_MORE_DOCS;
 use crate::core::util::error::lucene_error::Result;
 use crate::test::index::random_index_writer::RandomIndexWriter;
 use crate::test::util::lucene_test_case::lucene_test_case_util::{
-    at_least, new_bytes_ref_from_string, new_directory, new_field, new_index_writer_config, random,
+    at_least, get_only_leaf_reader, new_bytes_ref_from_string, new_directory, new_field,
+    new_index_writer_config, new_text_field, random,
 };
 use rand::Rng;
 use std::sync::Arc;
@@ -116,6 +119,30 @@ fn test_positions_simple() -> Result<()> {
     }
     Ok(())
 }
+fn get_docs_and_positions<LR>(
+    reader: &LR,
+    field_name: &str,
+    bytes: &BytesRef<Vec<u8>>,
+) -> Result<Option<LRPosting<LR>>>
+where
+    LR: LeafReader,
+{
+    let terms_opt = reader.terms(field_name)?;
+    let terms = match terms_opt {
+        None => return Ok(None),
+        Some(t) => t,
+    };
+
+    let mut te = terms.iterator()?;
+
+    if te.seek_exact(bytes)? {
+        let pe = te.postings_with_flags(None, ALL as i32)?;
+        Ok(Some(pe))
+    } else {
+        Ok(None)
+    }
+}
+
 /// this test indexes random numbers within a range into a field and checks their occurrences by
 /// searching for a number from that range selected at random. All positions for that number are
 /// saved up front and compared to the enums positions.
@@ -238,27 +265,139 @@ fn test_random_positions() -> Result<()> {
 
     Ok(())
 }
+#[test]
+fn test_random_docs() -> Result<()> {
+    // TODO MultiTerm未实现
+    Ok(())
+}
+/// tests retrieval of positions for terms that have a large number of occurrences to force test of
+//  buffer refill during positions iteration.
+#[test]
+fn test_large_number_of_positions() -> Result<()> {
+    let mut random = random();
+    let directory = Arc::new(new_directory(&mut random)?);
 
-fn get_docs_and_positions<LR>(
-    reader: &LR,
-    field_name: &str,
-    bytes: &BytesRef<Vec<u8>>,
-) -> Result<Option<LRPosting<LR>>>
-where
-    LR: LeafReader,
-{
-    let terms_opt = reader.terms(field_name)?;
-    let terms = match terms_opt {
-        None => return Ok(None),
-        Some(t) => t,
-    };
+    // TODO: 未实现 MockAnalyzer
+    let config = new_index_writer_config(&mut random);
+    let writer = RandomIndexWriter::with_config(&mut random, directory.clone(), config);
 
+    let field_name = field_name(&mut random);
+
+    let how_many = 1000;
+
+    let mut custom_type = FieldType::from_ref(&text_field_type::TYPE_NOT_STORED.clone())?;
+    custom_type.set_omit_norms(true)?;
+
+    for _i in 0..39 {
+        let mut doc = Document::new();
+        let mut builder = String::new();
+
+        for j in 0..how_many {
+            if j % 2 == 0 {
+                builder.push_str("even ");
+            } else {
+                builder.push_str("odd ");
+            }
+        }
+
+        doc.add(new_field(&field_name, builder, &custom_type)?);
+        writer.add_document(doc)?;
+    }
+    // now do searches
+    let reader = Arc::new(writer.get_reader()?);
+    writer.close()?;
+
+    let num_outer = at_least(&mut random, 13);
+
+    for i in 0..num_outer {
+        let bytes = new_bytes_ref_from_string(&mut random, "even")?;
+        let top_reader_context = get_context(reader.clone())?;
+
+        for leaf_ctx in top_reader_context.leaves()? {
+            let leaf_reader = leaf_ctx.reader();
+
+            let mut docs_and_pos_enum = get_docs_and_positions(leaf_reader, &field_name, &bytes)?
+                .expect("postings enum must exist");
+
+            let max_doc = leaf_reader.max_doc()?;
+            if max_doc == 0 {
+                continue;
+            }
+
+            // initially advance or do next doc
+            let init_doc = if random.random_bool(0.5) {
+                docs_and_pos_enum.next_doc()?
+            } else {
+                docs_and_pos_enum.advance(random.random_range(0..max_doc))?
+            };
+
+            let msg = format!("Iteration: {} initDoc: {}", i, init_doc);
+
+            assert_eq!(how_many / 2, docs_and_pos_enum.freq()?, "{msg}");
+
+            for j in (0..how_many).step_by(2) {
+                let pos = docs_and_pos_enum.next_position()?;
+                assert_eq!(
+                    j,
+                    pos,
+                    "position missmatch index: {} with freq: {} -- {}",
+                    j,
+                    docs_and_pos_enum.freq()?,
+                    msg
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+fn test_docs_enum_start() -> Result<()> {
+    // TODO MultiTerm未实现
+    Ok(())
+}
+#[test]
+fn test_docs_and_positions_enum_start() -> Result<()> {
+    let mut random = random();
+    let directory = Arc::new(new_directory(&mut random)?);
+
+    let config = new_index_writer_config(&mut random);
+    let writer = RandomIndexWriter::with_config(&mut random, directory.clone(), config);
+
+    let mut doc = Document::new();
+    doc.add(new_text_field("foo", "bar", No)?);
+    writer.add_document(doc)?;
+
+    let reader = Arc::new(writer.get_reader()?);
+    writer.close()?;
+
+    let r = get_only_leaf_reader(reader.clone())?;
+
+    let term = Term::from_text("foo", "bar");
+    let mut disi = r.postings_with_flag(&term, ALL as i32)?.unwrap();
+    let docid = disi.doc_id();
+    assert_eq!(-1, docid);
+
+    let next = disi.next_doc()?;
+    assert_ne!(next, NO_MORE_DOCS);
+    // now reuse and check again
+    let terms = r.terms("foo")?.unwrap();
     let mut te = terms.iterator()?;
 
-    if te.seek_exact(bytes)? {
-        let pe = te.postings_with_flags(None, ALL as i32)?;
-        Ok(Some(pe))
-    } else {
-        Ok(None)
+    assert!(te.seek_exact(&new_bytes_ref_from_string(&mut random, "bar")?)?);
+    match disi {
+        Either2PostingsEnum::A(v) => {
+            let mut disi = te.postings_with_flags(Some(v), ALL as i32)?;
+
+            let docid = disi.doc_id();
+            assert_eq!(-1, docid);
+
+            let next2 = disi.next_doc()?;
+            assert_ne!(next2, NO_MORE_DOCS);
+        },
+        Either2PostingsEnum::B(v) => {
+            assert!(false, "should not happen");
+        },
     }
+
+    Ok(())
 }
