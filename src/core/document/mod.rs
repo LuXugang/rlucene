1c52bfc0949115b0b3519da671aa63ccf7c31be2
Lu Xugang <luxugang@apache.org>
2025-11-26 17:05:55 +0800

    Implement DocValuesLongHashSet

--- src/core/document/mod.rs
+++ src/core/document/mod.rs
@@ -16,6 +16,7 @@
  */
 pub mod binary_doc_values_field;
 pub mod binary_point;
+pub mod doc_values_long_hash_set;
 #[allow(clippy::module_inception)]
 pub mod document;
 pub mod document_stored_field_visitor;
