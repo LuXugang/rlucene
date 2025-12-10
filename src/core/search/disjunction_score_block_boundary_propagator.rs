8e554274c8d87d9a20f1f9605a409d812f0357d1
Lu Xugang <luxugang@apache.org>
2025-12-10 20:14:55 +0800

    Implement TestDisjunctionScoreBlockBoundaryPropagator

--- src/core/search/disjunction_score_block_boundary_propagator.rs
+++ src/core/search/disjunction_score_block_boundary_propagator.rs
@@ -47,7 +47,7 @@ where
             cost.push((max_score, i, iter_cost));
             tmp_scorers.push(Some(scorer));
         }
-        cost.sort_by(|a, b| b.0.total_cmp(&a.0).then_with(|| b.2.cmp(&a.2)));
+        cost.sort_by(|a, b| a.0.total_cmp(&b.0).then_with(|| a.2.cmp(&b.2)));
         let mut scorers = Vec::with_capacity(cost.len());
         for (max_score, idx, _) in cost.into_iter() {
             let s = tmp_scorers[idx].take().unwrap();
@@ -108,6 +108,129 @@ where
             && min_score > self.max_scores[self.lead_index as usize]
         {
             self.lead_index += 1;
+        }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::core::search::disjunction_score_block_boundary_propagator::DisjunctionScoreBlockBoundaryPropagator;
+
+    use crate::core::search::doc_id_set_iterator::EmptyDISI;
+    use crate::core::search::dummy::dummy_scorable::DummyScorable;
+    use crate::core::search::dummy::dummy_two_phase_iterator::DummyTwoPhaseIterator;
+    use crate::core::search::scorable::Scorable;
+    use crate::core::search::scorer::{Scorer, TwoPhaseState};
+    use crate::core::util::error::lucene_error::{LuceneError, Result};
+    use crate::test::util::lucene_test_case::lucene_test_case_util::random;
+    use rand::prelude::SliceRandom;
+
+    #[allow(dead_code)] // for quick search
+    struct TestDisjunctionScoreBlockBoundaryPropagator;
+    #[test]
+    fn test_basics() -> Result<()> {
+        let mut random = random();
+        let scorer1 = FakeScorer::new(20, 0.5);
+        let scorer2 = FakeScorer::new(50, 1.5);
+        let scorer3 = FakeScorer::new(30, 2.0);
+        let scorer4 = FakeScorer::new(80, 3.0);
+        let mut scorers = vec![scorer1, scorer2, scorer3, scorer4];
+        scorers.shuffle(&mut random);
+
+        let mut propagator = DisjunctionScoreBlockBoundaryPropagator::new(scorers)?;
+
+        assert_eq!(20, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(0.2);
+        assert_eq!(20, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(0.7);
+        assert_eq!(30, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(1.2);
+        assert_eq!(30, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(1.7);
+        assert_eq!(30, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(2.2);
+        assert_eq!(80, propagator.advance_shallow(0)?);
+
+        propagator.set_min_competitive_score(5.0);
+        assert_eq!(80, propagator.advance_shallow(0)?);
+
+        Ok(())
+    }
+
+    struct FakeScorer {
+        boundary: i32,
+        max_score: f32,
+        disi: EmptyDISI,
+    }
+    impl FakeScorer {
+        fn new(boundary: i32, max_score: f32) -> Self {
+            Self {
+                boundary,
+                max_score,
+                disi: EmptyDISI::default(),
+            }
+        }
+    }
+
+    impl Scorable for FakeScorer {
+        fn score(&mut self) -> Result<f32> {
+            Err(LuceneError::unsupported_operation(""))
+        }
+
+        type Scorable = DummyScorable;
+    }
+
+    impl Scorer for FakeScorer {
+        type DocIdSetIterator = EmptyDISI;
+        type DocIdSetIteratorRef<'a>
+            = &'a EmptyDISI
+        where
+            Self: 'a;
+        type DocIdSetIteratorMut<'a>
+            = &'a mut EmptyDISI
+        where
+            Self: 'a;
+        type TwoPhaseIter = DummyTwoPhaseIterator;
+        type TwoPhaseIterRef<'a>
+            = DummyTwoPhaseIterator
+        where
+            Self: 'a;
+        type TwoPhaseIterMut<'a>
+            = DummyTwoPhaseIterator
+        where
+            Self: 'a;
+
+        fn doc_id(&mut self) -> Result<i32> {
+            Ok(0)
+        }
+
+        fn iterator(&self) -> Self::DocIdSetIteratorRef<'_> {
+            &self.disi
+        }
+
+        fn iterator_mut(&mut self) -> Self::DocIdSetIteratorMut<'_> {
+            &mut self.disi
+        }
+
+        fn take_iterator(self) -> Self::DocIdSetIterator {
+            self.disi
+        }
+
+        fn advance_shallow(&mut self, target: i32) -> Result<i32> {
+            assert!(target <= self.boundary);
+            Ok(self.boundary)
+        }
+
+        fn get_max_score(&mut self, _up_to: i32) -> Result<f32> {
+            Ok(self.max_score)
+        }
+
+        fn has_two_phase_iterator(&self) -> TwoPhaseState {
+            TwoPhaseState::No
         }
     }
 }
