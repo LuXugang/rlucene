30148017e2f65e6a8a67f6017293aee4f5d5e4d3
Lu Xugang <luxugang@apache.org>
2026-01-14 19:02:51 +0800

    Change `Bits::get` to return `Result<bool>` and propagate fallible access (#116)
    * Update Bits get to return Result
    
    * Code tidy

--- src/core/search/match_all_docs_query.rs
+++ src/core/search/match_all_docs_query.rs
@@ -262,7 +262,10 @@ impl BulkScorer for MatchAllBulkScorer {
         let mut scorer = Score::new(self.score);
         collector.set_scorer(&mut scorer)?;
         for doc in min..max {
-            if accept_docs.is_none_or(|bits| bits.get(doc as usize)) {
+            if match accept_docs {
+                None => true,
+                Some(bits) => bits.get(doc as usize)?,
+            } {
                 collector.collect(doc, &mut scorer)?;
             }
         }
