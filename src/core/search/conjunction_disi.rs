56a6bf9fd22c04095f13aeb140c00929753d1432
Lu Xugang <luxugang@apache.org>
2026-01-28 03:04:28 +0800

    Refactor ConjunctionDISI

--- src/core/search/conjunction_disi.rs
+++ src/core/search/conjunction_disi.rs
@@ -28,9 +28,10 @@ pub struct ConjunctionDISI<D>
 where
     D: DocIdSetIterator,
 {
-    lead1: D,
-    lead2: D,
-    others: Vec<D>,
+    lead1: usize,
+    lead2: usize,
+    others: Vec<usize>,
+    all_disi: Vec<D>,
 }
 impl<D> ConjunctionDISI<D>
 where
@@ -39,16 +40,14 @@ where
     fn new(iterators: Vec<D>) -> Result<Self> {
         debug_assert!(iterators.len() >= 2);
         let mut cost = Vec::with_capacity(iterators.len());
-        let mut temp_iterators = Vec::with_capacity(iterators.len());
-        for (idx, v) in iterators.into_iter().enumerate() {
+        for idx in 0..iterators.len() {
             cost.push(idx);
-            temp_iterators.push(Some(v));
         }
-        let cmp = DisiCmp::new(temp_iterators.as_ref());
+        let cmp = DisiCmp::new(iterators.as_ref());
         CollectionUtil::tim_sort_with_comparator(&mut cost, cmp)?;
-        let mut iters = Vec::with_capacity(temp_iterators.len());
+        let mut iters = Vec::with_capacity(iterators.len());
         for idx in cost {
-            iters.push(temp_iterators[idx].take().unwrap());
+            iters.push(idx);
         }
         let lead1 = iters.remove(0);
         let lead2 = iters.remove(0);
@@ -56,23 +55,25 @@ where
             lead1,
             lead2,
             others: iters,
+            all_disi: iterators,
         })
     }
     fn do_next(&mut self, mut doc: i32) -> Result<i32> {
         'advance_head: loop {
-            debug_assert_eq!(doc, self.lead1.doc_id());
+            debug_assert_eq!(doc, self.all_disi[self.lead1].doc_id());
             // find agreement between the two iterators with the lower costs
             // we special case them because they do not need the
             // 'other.docID() < doc' check that the 'others' iterators need
-            let next2 = self.lead2.advance(doc)?;
+            let next2 = self.all_disi[self.lead2].advance(doc)?;
             if next2 != doc {
-                doc = self.lead1.advance(next2)?;
+                doc = self.all_disi[self.lead1].advance(next2)?;
                 if doc != next2 {
                     continue;
                 }
             }
             // then find agreement with other iterators
-            for other in &mut self.others {
+            for other_idx in self.others.iter() {
+                let other = &mut self.all_disi[*other_idx];
                 let other_doc = other.doc_id();
                 // other.doc may already be equal to doc if we "continued advanceHead"
                 // on the previous iteration and the advance on the lead scorer exactly matched.
@@ -81,7 +82,7 @@ where
 
                     if next > doc {
                         // iterator beyond the current doc - advance lead and continue to the new highest doc.
-                        doc = self.lead1.advance(next)?;
+                        doc = self.all_disi[self.lead1].advance(next)?;
                         continue 'advance_head;
                     }
                 }
@@ -91,12 +92,12 @@ where
     }
     // Returns {@code true} if all sub-iterators are on the same doc ID, {@code false} otherwise
     fn assert_iters_on_same_doc(&self) -> bool {
-        let cur_doc = self.lead1.doc_id();
-        let mut iterators_on_the_same_doc = self.lead2.doc_id() == cur_doc;
+        let cur_doc = self.all_disi[self.lead1].doc_id();
+        let mut iterators_on_the_same_doc = self.all_disi[self.lead2].doc_id() == cur_doc;
         let mut i = 0;
         while i < self.others.len() && iterators_on_the_same_doc {
             iterators_on_the_same_doc =
-                iterators_on_the_same_doc && (self.others[i].doc_id() == cur_doc);
+                iterators_on_the_same_doc && (self.all_disi[self.others[i]].doc_id() == cur_doc);
             i += 1;
         }
         iterators_on_the_same_doc
@@ -107,7 +108,7 @@ where
     D: DocIdSetIterator,
 {
     fn doc_id(&self) -> i32 {
-        self.lead1.doc_id()
+        self.all_disi[self.lead1].doc_id()
     }
 
     fn next_doc(&mut self) -> Result<i32> {
@@ -115,7 +116,7 @@ where
             self.assert_iters_on_same_doc(),
             "Sub-iterators of ConjunctionDISI are not on the same document!"
         );
-        let doc = self.lead1.next_doc()?;
+        let doc = self.all_disi[self.lead1].next_doc()?;
         self.do_next(doc)
     }
 
@@ -124,12 +125,12 @@ where
             self.assert_iters_on_same_doc(),
             "Sub-iterators of ConjunctionDISI are not on the same document!"
         );
-        let doc = self.lead1.advance(target)?;
+        let doc = self.all_disi[self.lead1].advance(target)?;
         self.do_next(doc)
     }
 
     fn cost(&self) -> Result<i64> {
-        self.lead1.cost()
+        self.all_disi[self.lead1].cost()
     }
 }
 
@@ -137,13 +138,13 @@ struct DisiCmp<'a, D>
 where
     D: DocIdSetIterator,
 {
-    disi: &'a [Option<D>],
+    disi: &'a [D],
 }
 impl<'a, D> DisiCmp<'a, D>
 where
     D: DocIdSetIterator,
 {
-    fn new(disi: &'a [Option<D>]) -> Self {
+    fn new(disi: &'a [D]) -> Self {
         DisiCmp { disi }
     }
 }
@@ -154,12 +155,7 @@ where
     const TYPE: &'static str = "DisiCmp";
 
     fn compare(&self, a: &usize, b: &usize) -> Result<i32> {
-        Ok(self.disi[*a]
-            .as_ref()
-            .unwrap()
-            .cost()?
-            .cmp(&self.disi[*b].as_ref().unwrap().cost()?)
-            .to_int())
+        Ok(self.disi[*a].cost()?.cmp(&self.disi[*b].cost()?).to_int())
     }
 }
 /// Conjunction between a [`DocIdSetIterator`] and one or more BitSetIterators.
