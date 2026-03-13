03d3a66ec3547a55ebf3dae83373fe737bb5b718
Lu Xugang <luxugang@apache.org>
2026-03-13 11:39:12 +0800

    Implement TestNorms

--- src/test/index/mod.rs
+++ src/test/index/mod.rs
@@ -36,6 +36,7 @@ mod test_index_writer_delete;
 mod test_index_writer_max_docs;
 mod test_many_fields;
 mod test_non_flex;
+mod test_norms;
 mod test_omit_norms;
 mod test_segment_term_docs;
 mod test_segment_term_enum;
