e52bdc3289ef89158b61ebcc8978cced2678e8b8
Lu Xugang <luxugang@apache.org>
2026-03-17 15:48:13 +0800

    Implement TestTotalHitCountCollector

--- src/core/search/total_hit_count_collector.rs
+++ src/core/search/total_hit_count_collector.rs
@@ -105,3 +105,77 @@ impl<'a> LeafCollector for TotalHitCountLeafCollector<'a> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::core::document::document::Document;
+    use crate::core::document::field::Store;
+    use crate::core::document::string_field::StringField;
+
+    use crate::core::index::term::Term;
+    use crate::core::search::boolean_clause::Occur;
+    use crate::core::search::boolean_query::Builder;
+
+    use crate::core::search::match_all_docs_query::MatchAllDocsQuery;
+
+    use crate::core::search::term_query::TermQuery;
+    use crate::core::search::total_hit_count_collector_manager::TotalHitCountCollectorManager;
+
+    use crate::core::util::error::lucene_error::Result;
+    use crate::test::core::index::random_index_writer::RandomIndexWriter;
+    use crate::test::core::util::lucene_test_case::lucene_test_case_util::{
+        new_directory_shared, new_searcher_with_reader, random,
+    };
+
+    #[allow(dead_code)] // for quick search
+    struct TestTotalHitCountCollector;
+
+    #[test]
+    fn test_basics() -> Result<()> {
+        let mut random = random();
+        let index_store = new_directory_shared(&mut random)?;
+        let writer = RandomIndexWriter::new(&mut random, index_store.clone());
+
+        for i in 0..5 {
+            let mut doc = Document::new();
+            doc.add(StringField::from_string(
+                "string",
+                format!("a{}", i),
+                Store::No,
+            )?);
+            doc.add(StringField::from_string(
+                "string",
+                format!("b{}", i),
+                Store::No,
+            )?);
+            writer.add_document(doc)?;
+        }
+
+        let reader = writer.get_reader()?;
+        writer.close()?;
+
+        // TODO IMPORTANT 多线程未实现
+        let searcher = new_searcher_with_reader(reader)?;
+        let collector_manager =
+            TotalHitCountCollectorManager::new(searcher.get_slices()?.as_slice());
+        let mut total_hits =
+            searcher.search_with_collector_manager(MatchAllDocsQuery::new(), &collector_manager)?;
+        assert_eq!(5, total_hits);
+
+        let mut builder = Builder::new();
+        builder.add(
+            TermQuery::new(Term::from_text("string", "a1")),
+            Occur::Should,
+        )?;
+        builder.add(
+            TermQuery::new(Term::from_text("string", "b3")),
+            Occur::Should,
+        )?;
+        let query = builder.build();
+
+        total_hits = searcher.search_with_collector_manager(query, &collector_manager)?;
+        assert_eq!(2, total_hits);
+
+        Ok(())
+    }
+}
