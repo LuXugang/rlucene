a3b5d1bb047d1ae226a4743b3c08ae84082954e4
Lu Xugang <luxugang@apache.org>
2026-04-17 23:12:08 +0800

    implement  TestTopDocsMerge

--- src/test/core/search/mod.rs
+++ src/test/core/search/mod.rs
@@ -45,4 +45,5 @@ mod test_search_after;
 mod test_similarity_provider;
 mod test_sort_optimization;
 pub(crate) mod test_sort_random;
+mod test_top_docs_merge;
 mod test_top_field_collector_early_termination;
