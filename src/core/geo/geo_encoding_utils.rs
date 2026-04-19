8988d20bce30e766c03bccbe2e8a8a3a78bb9a04
Lu Xugang <luxugang@apache.org>
2026-04-20 05:12:49 +0800

    Add TestGeoEncodingUtils

--- src/core/geo/geo_encoding_utils.rs
+++ src/core/geo/geo_encoding_utils.rs
@@ -390,3 +390,166 @@ where
     }
   }
 }
+#[cfg(test)]
+mod tests {
+  use crate::core::geo::geo_encoding_utils::GeoEncodingUtils;
+  use crate::core::geo::geo_utils::GeoUtils;
+  use crate::core::util::error::lucene_error::Result;
+  use crate::core::util::numeric_utils::NumericUtils;
+  use crate::test::core::util::lucene_test_case::lucene_test_case_util::random;
+  use crate::test::core::util::test_util::TestUtil;
+  use rand::RngExt;
+
+  #[allow(dead_code)] // for quick search
+  struct TestGeoEncodingUtils;
+  #[test]
+  fn test_latitude_quantization() -> Result<()> {
+    let latitude_decode = 180.0f64 / ((1u64 << 32) as f64);
+    let mut random = random();
+
+    for _ in 0..10000 {
+      let encoded: i32 = random.random();
+      let min =
+        GeoUtils::MIN_LAT_INCL + ((encoded as i64 - i32::MIN as i64) as f64) * latitude_decode;
+
+      let decoded = GeoEncodingUtils::decode_latitude(encoded);
+      assert_eq!(min, decoded);
+
+      assert_eq!(encoded, GeoEncodingUtils::encode_latitude(decoded)?);
+      assert_eq!(encoded, GeoEncodingUtils::encode_latitude_ceil(decoded)?);
+
+      if encoded != i32::MAX {
+        let max = min + latitude_decode;
+        assert_eq!(max, GeoEncodingUtils::decode_latitude(encoded + 1));
+        assert_eq!(encoded + 1, GeoEncodingUtils::encode_latitude(max)?);
+        assert_eq!(encoded + 1, GeoEncodingUtils::encode_latitude_ceil(max)?);
+
+        let min_edge = min.next_up();
+        let max_edge = max.next_down();
+
+        assert_eq!(encoded, GeoEncodingUtils::encode_latitude(min_edge)?);
+        assert_eq!(
+          encoded + 1,
+          GeoEncodingUtils::encode_latitude_ceil(min_edge)?
+        );
+        assert_eq!(encoded, GeoEncodingUtils::encode_latitude(max_edge)?);
+        assert_eq!(
+          encoded + 1,
+          GeoEncodingUtils::encode_latitude_ceil(max_edge)?
+        );
+
+        let min_bits = NumericUtils::double_to_sortable_long(min_edge);
+        let max_bits = NumericUtils::double_to_sortable_long(max_edge);
+
+        for _ in 0..100 {
+          let value = NumericUtils::sortable_long_to_double(TestUtil::next_long(
+            &mut random,
+            min_bits,
+            max_bits,
+          ));
+
+          assert_eq!(encoded, GeoEncodingUtils::encode_latitude(value)?);
+          assert_eq!(encoded + 1, GeoEncodingUtils::encode_latitude_ceil(value)?);
+        }
+      }
+    }
+
+    Ok(())
+  }
+  /// step through some integers, ensuring they decode to their expected double values. double values
+  /// start at -180 and increase by LONGITUDE_DECODE for each integer. check edge cases within the
+  /// double range and a random doubles within the range too.
+  #[test]
+  fn test_longitude_quantization() -> Result<()> {
+    let longitude_decode = 360.0f64 / ((1u64 << 32) as f64);
+    let mut random = random();
+
+    for _ in 0..10000 {
+      let encoded: i32 = random.random();
+      let min =
+        GeoUtils::MIN_LON_INCL + ((encoded as i64 - i32::MIN as i64) as f64) * longitude_decode;
+
+      let decoded = GeoEncodingUtils::decode_longitude(encoded);
+      assert_eq!(min, decoded);
+
+      assert_eq!(encoded, GeoEncodingUtils::encode_longitude(decoded)?);
+      assert_eq!(encoded, GeoEncodingUtils::encode_longitude_ceil(decoded)?);
+
+      if encoded != i32::MAX {
+        let max = min + longitude_decode;
+        assert_eq!(max, GeoEncodingUtils::decode_longitude(encoded + 1));
+        assert_eq!(encoded + 1, GeoEncodingUtils::encode_longitude(max)?);
+        assert_eq!(encoded + 1, GeoEncodingUtils::encode_longitude_ceil(max)?);
+
+        let min_edge = min.next_up();
+        let max_edge = max.next_down();
+
+        assert_eq!(encoded, GeoEncodingUtils::encode_longitude(min_edge)?);
+        assert_eq!(
+          encoded + 1,
+          GeoEncodingUtils::encode_longitude_ceil(min_edge)?
+        );
+        assert_eq!(encoded, GeoEncodingUtils::encode_longitude(max_edge)?);
+        assert_eq!(
+          encoded + 1,
+          GeoEncodingUtils::encode_longitude_ceil(max_edge)?
+        );
+
+        let min_bits = NumericUtils::double_to_sortable_long(min_edge);
+        let max_bits = NumericUtils::double_to_sortable_long(max_edge);
+
+        for _ in 0..100 {
+          let value = NumericUtils::sortable_long_to_double(TestUtil::next_long(
+            &mut random,
+            min_bits,
+            max_bits,
+          ));
+
+          assert_eq!(encoded, GeoEncodingUtils::encode_longitude(value)?);
+          assert_eq!(encoded + 1, GeoEncodingUtils::encode_longitude_ceil(value)?);
+        }
+      }
+    }
+
+    Ok(())
+  }
+
+  #[test]
+  fn test_encode_edge_cases() -> Result<()> {
+    assert_eq!(
+      i32::MIN,
+      GeoEncodingUtils::encode_latitude(GeoUtils::MIN_LAT_INCL)?
+    );
+    assert_eq!(
+      i32::MIN,
+      GeoEncodingUtils::encode_latitude_ceil(GeoUtils::MIN_LAT_INCL)?
+    );
+    assert_eq!(
+      i32::MAX,
+      GeoEncodingUtils::encode_latitude(GeoUtils::MAX_LAT_INCL)?
+    );
+    assert_eq!(
+      i32::MAX,
+      GeoEncodingUtils::encode_latitude_ceil(GeoUtils::MAX_LAT_INCL)?
+    );
+
+    assert_eq!(
+      i32::MIN,
+      GeoEncodingUtils::encode_longitude(GeoUtils::MIN_LON_INCL)?
+    );
+    assert_eq!(
+      i32::MIN,
+      GeoEncodingUtils::encode_longitude_ceil(GeoUtils::MIN_LON_INCL)?
+    );
+    assert_eq!(
+      i32::MAX,
+      GeoEncodingUtils::encode_longitude(GeoUtils::MAX_LON_INCL)?
+    );
+    assert_eq!(
+      i32::MAX,
+      GeoEncodingUtils::encode_longitude_ceil(GeoUtils::MAX_LON_INCL)?
+    );
+
+    Ok(())
+  }
+}
