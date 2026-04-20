77dfe5676989571939152c8da0767b1433a5e40d
Lu Xugang <luxugang@apache.org>
2026-04-20 21:39:15 +0800

    Implement either_lat_lon_geometry_named

--- src/core/geo/lat_lon_geometry.rs
+++ src/core/geo/lat_lon_geometry.rs
@@ -14,10 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use crate::core::geo::circle::Circle;
 use crate::core::geo::component_tree::{ComponentTree, component_tree_util};
 use crate::core::geo::component2d::Component2DEnum2;
+use crate::core::geo::component2d::{Component2D, WithinRelation};
 use crate::core::geo::geometry::Geometry;
+use crate::core::geo::line::Line;
+use crate::core::geo::point::Point;
+use crate::core::geo::polygon::Polygon;
+use crate::core::geo::rectangle::Rectangle;
+use crate::core::index::point_values::Relation;
 use crate::core::util::error::lucene_error::{LuceneError, Result};
+use crate::impl_from_for_enum;
 
 /// Lat/Lon Geometry object.
 pub trait LatLonGeometry: Geometry {}
@@ -44,3 +52,60 @@ where
     components,
   )?))
 }
+#[macro_export]
+macro_rules! either_lat_lon_geometry_named {
+    (
+        $vis:vis $name:ident,
+        $component_name:ident {
+            $( $Variant:ident : $T:ident ),+ $(,)?
+        }
+    ) => {
+        $crate::either_component2d_named!(
+            $vis $component_name {
+                $( $Variant : $T ),+
+            }
+        );
+
+        $vis enum $name<$( $T ),+> {
+            $( $Variant($T), )+
+        }
+
+        impl<$( $T ),+> Geometry for $name<$( $T ),+>
+        where
+            $( $T: LatLonGeometry ),+
+        {
+            type Component2D = $component_name<$( <$T as Geometry>::Component2D ),+>;
+
+            fn to_component2d(&self) -> Result<Self::Component2D> {
+                match self {
+                    $( Self::$Variant(inner) => Ok(Self::Component2D::$Variant(inner.to_component2d()?)), )+
+                }
+            }
+        }
+
+        impl<$( $T ),+> LatLonGeometry for $name<$( $T ),+>
+        where
+            $( $T: LatLonGeometry ),+
+        {}
+    };
+}
+
+either_lat_lon_geometry_named!(
+  pub LatLonGeometryEnum5,
+  LatLonGeometryComponent2DEnum5 {
+    Circle: A,
+    Line: B,
+    Point: C,
+    Polygon: D,
+    Rectangle: E,
+  }
+);
+pub type LatLonGeometryEnum = LatLonGeometryEnum5<Circle, Line, Point, Polygon, Rectangle>;
+impl_from_for_enum!(
+LatLonGeometryEnum,
+Circle => Circle,
+Line => Line,
+Point => Point,
+Polygon => Polygon,
+Rectangle => Rectangle,
+);
