29ccd7c9a56d4d10cc3af6b5294f5e4d0d56f01b
Lu Xugang <luxugang@apache.org>
2026-04-24 21:24:41 +0800

    Implement TestLatLonPointDistanceFeatureQuery

--- src/test/core/document/mod.rs
+++ src/test/core/document/mod.rs
@@ -16,6 +16,5 @@
  */
 mod base_shape_encoding_test_case;
 pub(crate) mod base_spatial_test_case;
-mod test_lat_lon_point_distance_feature_query;
 mod test_lat_lon_shape_encoding;
 mod test_xy_shape_encoding;
