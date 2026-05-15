f1a535692d127b052b866f27ac30378d39f0ec8b
Lu Xugang <luxugang@apache.org>
2026-05-15 16:58:14 +0800

    Implement TestNewestSegment

--- src/test/core/index/mod.rs
+++ src/test/core/index/mod.rs
@@ -68,6 +68,7 @@ mod test_many_fields;
 mod test_max_position;
 mod test_merge_on_flush_merge_policy;
 mod test_never_delete;
+mod test_newest_segment;
 mod test_no_merge_policy;
 mod test_non_flex;
 mod test_norms;
