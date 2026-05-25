0511cd7db8c02d90433f73fa0e751d73a1b18fc0
Lu Xugang <luxugang@apache.org>
2026-05-25 22:08:29 +0800

    Implement TestFilterIndexInput

---
+++ src/test/core/index/test_filter_index_input.rs
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use crate::core::util::error::lucene_error::Result;
+#[allow(dead_code)] // for quick search
+struct TestFilterIndexInput;
+#[test]
+fn test_raw_filter_index_input_read() -> Result<()> {
+  test_not_required_in_rust_lucene!();
+}
+
+#[test]
+fn test_overrides() -> Result<()> {
+  test_not_required_in_rust_lucene!();
+}
+
+#[test]
+fn test_unwrap() -> Result<()> {
+  test_not_required_in_rust_lucene!();
+}
