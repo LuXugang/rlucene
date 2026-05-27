a55523ef38d8c6b0675680174eddc6c432fa9dab
Lu Xugang <luxugang@apache.org>
2026-05-27 10:55:19 +0800

    Prepare for TestIndexWriterFromReader

--- src/test/core/index/test_index_writer_from_reader.rs
+++ src/test/core/index/test_index_writer_from_reader.rs
@@ -15,5 +15,67 @@
  * limitations under the License.
  */
 
+use crate::core::util::error::lucene_error::Result;
+
 #[allow(dead_code)] // for quick search
 struct TestIndexWriterFromReader;
+
+#[test]
+fn test_right_after_commit() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_from_non_nrt_reader() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_with_no_first_commit() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_after_commit_then_index() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_nrt_rollback() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_random() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_consistent_field_numbers() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_invalid_open_mode() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_on_closed_reader() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_stale_nrt_reader() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_after_rollback() -> Result<()> {
+  Ok(())
+}
+
+#[test]
+fn test_after_commit_then_index_keep_commits() -> Result<()> {
+  Ok(())
+}
